175255e66045269fbff7f8ddec6040a2a305f752
Daekeun Ko <kdkcom1234@gmail.com>
2024-02-07 16:47:34 +0900

    로컬파일 컨피그 서버까지 완료

--- configserver/src/main/java/com/optimagrowth/configserver/ConfigurationServerApplication.java
+++ configserver/src/main/java/com/optimagrowth/configserver/ConfigurationServerApplication.java
@@ -2,8 +2,11 @@ package com.optimagrowth.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+// 스프링 클라우드 컨피그 서비스 활성화
+@EnableConfigServer
 public class ConfigurationServerApplication {
 
 	public static void main(String[] args) {
