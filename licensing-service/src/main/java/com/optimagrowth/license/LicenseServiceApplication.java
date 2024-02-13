d910ba4942a3ed06c9ab202af73949e2c0c9385d
Daekeun Ko <kdkcom1234@gmail.com>
2024-02-13 15:04:36 +0900

    postgresql localhost

--- licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 // 컨피그 서버에서 설정이 바귀면 스프링 클라우드 버스를 통해 게시하는 방법 존재
 // 컨피그를 수정하면 디스커버리 서버에 질의하여 서비스의 모든 인스턴스를 찾고 /refresh를 호출하는 스크립트를 작성하는 방법도 가능
 @RefreshScope
+// eureka discovery client 사용
 @EnableDiscoveryClient
 public class LicenseServiceApplication {
 
