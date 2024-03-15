1953815f86787686fa2c7192976eb1d2e61f1b82
Daekeun Ko <kdkcom1234@gmail.com>
2024-03-15 16:23:41 +0900

    rest template interceptor 추가

--- licensing-service/src/main/java/com/optimagrowth/license/config/RestConfig.java
+++ licensing-service/src/main/java/com/optimagrowth/license/config/RestConfig.java
@@ -1,16 +1,30 @@
 package com.optimagrowth.license.config;
 
+import com.optimagrowth.license.utils.UserContextInterceptor;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class RestConfig {
     // 로드 밸런스가 사용되는 restTemplate
     @LoadBalanced
     @Bean
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+    public RestTemplate getRestTemplate(){
+        RestTemplate template = new RestTemplate();
+        List interceptors = template.getInterceptors();
+        if (interceptors==null){
+            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        }
+        else{
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+
+        return template;
     }
 }
