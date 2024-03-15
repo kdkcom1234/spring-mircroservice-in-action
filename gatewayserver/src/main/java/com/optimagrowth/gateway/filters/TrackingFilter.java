69478b0ebe2c5d1283803f6f78323efad9974685
Daekeun Ko <kdkcom1234@gmail.com>
2024-03-15 15:16:03 +0900

    상관관계Id 트래킹 필터 추가

--- gatewayserver/src/main/java/com/optimagrowth/gateway/filters/TrackingFilter.java
+++ gatewayserver/src/main/java/com/optimagrowth/gateway/filters/TrackingFilter.java
@@ -1,2 +1,56 @@
-package com.optimagrowth.gateway.filters;public class TrackingFilter {
+package com.optimagrowth.gateway.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Order(1)
+@Component
+public class TrackingFilter implements GlobalFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(TrackingFilter.class);
+
+    @Autowired
+    FilterUtils filterUtils;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+
+        // 상관관계Id 존재여부 확인
+        if (isCorrelationIdPresent(requestHeaders)) {
+            System.out.println("not found");
+            logger.debug("tmx-correlation-id found in tracking filter: {}. ",
+                    filterUtils.getCorrelationId(requestHeaders));
+        } else {
+            // 상관관계Id가 없으면 생성 후 헤더에 추가
+            String correlationID = generateCorrelationId();
+            exchange = filterUtils.setCorrelationId(exchange, correlationID);
+            logger.debug("tmx-correlation-id generated in tracking filter: {}.", correlationID);
+        }
+
+        return chain.filter(exchange);
+    }
+
+
+    private boolean isCorrelationIdPresent(HttpHeaders requestHeaders) {
+        if (filterUtils.getCorrelationId(requestHeaders) != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private String generateCorrelationId() {
+        return java.util.UUID.randomUUID().toString();
+    }
+
 }
