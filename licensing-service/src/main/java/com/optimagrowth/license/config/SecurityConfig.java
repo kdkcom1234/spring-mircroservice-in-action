bf50b0826287e5aafde4fc5b694e5d48c3152ffa
Daekeun Ko <kdkcom1234@gmail.com>
2024-03-20 13:26:34 +0900

    컨피그 수정

---
+++ licensing-service/src/main/java/com/optimagrowth/license/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+
+package com.optimagrowth.organization.config;
+
+
+import com.optimagrowth.organization.utils.CustomJwtAuthenticationConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true) // @RolesAllowed 활성화
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(new CustomJwtAuthenticationConverter())
+                        )
+                );
+        return http.build();
+    }
+}
+
